72fa7375e2b013f786c1f015f32b1a281dde5377
zcy <zhouchunyi_gege@163.com>
2018-04-25 17:01:10 +0800

    add code reading comments

--- src/data_structure/DenseVector.java
+++ src/data_structure/DenseVector.java
@@ -26,279 +26,274 @@ import java.io.Serializable;
 
 /**
  * Data Structure: dense vector
- * 
+ *
  * @author guoguibing
- * 
  */
 public class DenseVector implements Serializable {
+    private static final long serialVersionUID = -2930574547913792430L;
 
-	private static final long serialVersionUID = -2930574547913792430L;
+    protected int size;
+    protected double[] data;
 
-	protected int size;
-	protected double[] data;
+    /**
+     * Construct a dense vector with a specific size
+     *
+     * @param size the size of vector
+     */
+    public DenseVector(int size) {
+        this.size = size;
+        data = new double[size];
+    }
 
-	/**
-	 * Construct a dense vector with a specific size
-	 * 
-	 * @param size
-	 *            the size of vector
-	 */
-	public DenseVector(int size) {
-		this.size = size;
-		data = new double[size];
-	}
+    /**
+     * Construct a dense vector by deeply copying data from a given array
+     */
+    public DenseVector(double[] array) {
+        this(array, true);
+    }
 
-	/**
-	 * Construct a dense vector by deeply copying data from a given array
-	 */
-	public DenseVector(double[] array) {
-		this(array, true);
-	}
+    /**
+     * Construct a dense vector by copying data from a given array
+     *
+     * @param array a given data array
+     * @param deep  whether to deep copy array data
+     */
+    public DenseVector(double[] array, boolean deep) {
+        this.size = array.length;
+        if (deep) {
+            data = new double[array.length];
+            for (int i = 0; i < size; i++)
+                data[i] = array[i];
+        } else {
+            data = array;
+        }
+    }
 
-	/**
-	 * Construct a dense vector by copying data from a given array
-	 * 
-	 * @param array
-	 *            a given data array
-	 * @param deep
-	 *            whether to deep copy array data
-	 */
-	public DenseVector(double[] array, boolean deep) {
-		this.size = array.length;
-		if (deep) {
-			data = new double[array.length];
-			for (int i = 0; i < size; i++)
-				data[i] = array[i];
-		} else {
-			data = array;
-		}
-	}
+    /**
+     * Construct a dense vector by deeply copying data from a given vector
+     */
+    public DenseVector(DenseVector vec) {
+        this(vec.data);
+    }
 
-	/**
-	 * Construct a dense vector by deeply copying data from a given vector
-	 */
-	public DenseVector(DenseVector vec) {
-		this(vec.data);
-	}
+    /**
+     * Make a deep copy of current vector
+     */
+    public DenseVector clone() {
+        return new DenseVector(this);
+    }
 
-	/**
-	 * Make a deep copy of current vector
-	 */
-	public DenseVector clone() {
-		return new DenseVector(this);
-	}
-	
-	public int size() {
-		return this.size;
-	}
-	
-	/**
-	 * Initialize a dense vector with Gaussian values
-	 */
-	public void init(double mean, double sigma) {
-		for (int i = 0; i < size; i++)
-			data[i] = Randoms.gaussian(mean, sigma);
-	}
+    public int size() {
+        return this.size;
+    }
 
-	/**
-	 * Initialize a dense vector with uniform values in (0, 1)
-	 */
-	public void init() {
-		for (int i = 0; i < size; i++)
-			data[i] = Randoms.uniform();
-	}
+    /**
+     * Initialize a dense vector with Gaussian values
+     */
+    public void init(double mean, double sigma) {
+        for (int i = 0; i < size; i++)
+            data[i] = Randoms.gaussian(mean, sigma);
+    }
 
-	/**
-	 * Initialize a dense vector with uniform values in (0, range)
-	 */
-	public void init(double range) {
-		for (int i = 0; i < size; i++)
-			data[i] = Randoms.uniform(0, range);
-	}
+    /**
+     * Initialize a dense vector with uniform values in (0, 1)
+     */
+    public void init() {
+        for (int i = 0; i < size; i++)
+            data[i] = Randoms.uniform();
+    }
 
-	/**
-	 * Get a value at entry [index]
-	 */
-	public double get(int idx) {
-		return data[idx];
-	}
+    /**
+     * Initialize a dense vector with uniform values in (0, range)
+     */
+    public void init(double range) {
+        for (int i = 0; i < size; i++)
+            data[i] = Randoms.uniform(0, range);
+    }
 
-	/**
-	 * @return vector's data
-	 */
-	public double[] getData() {
-		return data;
-	}
+    /**
+     * Get a value at entry [index]
+     */
+    public double get(int idx) {
+        return data[idx];
+    }
 
-	/**
-	 * @return mean of current vector
-	 */
-	public double mean() {
-		return Stats.mean(data);
-	}
-	
-	/**
-	 * @return summation of entries
-	 */
-	public double sum(){
-		return Stats.sum(data);
-	}
-	
-	/**
-	 * @return squared summation of entries
-	 */
-	public double squaredSum(){
-		double sum = 0;
-		for (int i = 0; i < data.length; i ++) {
-			sum += data[i] * data[i];
-		}
-		return sum;
-	}
-	
-	/**
-	 * Set a value to entry [index]
-	 */
-	public void set(int idx, double val) {
-		data[idx] = val;
-	}
+    /**
+     * @return vector's data
+     */
+    public double[] getData() {
+        return data;
+    }
 
-	/**
-	 * Set a value to all entries
-	 */
-	public void setAll(double val) {
-		for (int i = 0; i < size; i++)
-			data[i] = val;
-	}
+    /**
+     * @return mean of current vector
+     */
+    public double mean() {
+        return Stats.mean(data);
+    }
 
-	/**
-	 * Add a value to entry [index]
-	 */
-	public void add(int idx, double val) {
-		data[idx] += val;
-	}
+    /**
+     * @return summation of entries
+     */
+    public double sum() {
+        return Stats.sum(data);
+    }
 
-	/**
-	 * Substract a value from entry [index]
-	 */
-	public void minus(int idx, double val) {
-		data[idx] -= val;
-	}
+    /**
+     * @return squared summation of entries
+     */
+    public double squaredSum() {
+        double sum = 0;
+        for (int i = 0; i < data.length; i++) {
+            sum += data[i] * data[i];
+        }
+        return sum;
+    }
 
-	/**
-	 * @return a dense vector by adding a value to all entries of current vector
-	 */
-	public DenseVector add(double val) {
-		DenseVector result = new DenseVector(size);
+    /**
+     * Set a value to entry [index]
+     */
+    public void set(int idx, double val) {
+        data[idx] = val;
+    }
 
-		for (int i = 0; i < size; i++)
-			result.data[i] = this.data[i] + val;
+    /**
+     * Set a value to all entries
+     */
+    public void setAll(double val) {
+        for (int i = 0; i < size; i++)
+            data[i] = val;
+    }
 
-		return result;
-	}
+    /**
+     * Add a value to entry [index]
+     */
+    public void add(int idx, double val) {
+        data[idx] += val;
+    }
 
-	/**
-	 * @return a dense vector by substructing a value from all entries of current vector
-	 */
-	public DenseVector minus(double val) {
+    /**
+     * Substract a value from entry [index]
+     */
+    public void minus(int idx, double val) {
+        data[idx] -= val;
+    }
 
-		DenseVector result = new DenseVector(size);
+    /**
+     * @return a dense vector by adding a value to all entries of current vector
+     */
+    public DenseVector add(double val) {
+        DenseVector result = new DenseVector(size);
 
-		for (int i = 0; i < size; i++)
-			result.data[i] = this.data[i] - val;
+        for (int i = 0; i < size; i++)
+            result.data[i] = this.data[i] + val;
 
-		return result;
-	}
+        return result;
+    }
 
-	/**
-	 * @return a dense vector by scaling a value to all entries of current vector
-	 */
-	public DenseVector scale(double val) {
+    /**
+     * @return a dense vector by substructing a value from all entries of current vector
+     */
+    public DenseVector minus(double val) {
 
-		DenseVector result = new DenseVector(size);
-		for (int i = 0; i < size; i++)
-			result.data[i] = this.data[i] * val;
+        DenseVector result = new DenseVector(size);
 
-		return result;
-	}
-	
-	public void selfScale(double val) {
-		for (int i = 0; i < size; i ++)
-			this.data[i] = this.data[i] * val;
-	}
+        for (int i = 0; i < size; i++)
+            result.data[i] = this.data[i] - val;
 
-	/**
-	 * Do vector operation: {@code a + b}
-	 * 
-	 * @return a dense vector with results of {@code c = a + b}
-	 */
-	public DenseVector add(DenseVector vec) {
-		assert size == vec.size;
+        return result;
+    }
 
-		DenseVector result = new DenseVector(size);
-		for (int i = 0; i < result.size; i++)
-			result.data[i] = this.data[i] + vec.data[i];
+    /**
+     * @return a dense vector by scaling a value to all entries of current vector
+     */
+    public DenseVector scale(double val) {
 
-		return result;
-	}
-	
-	/**
-	 * Vector add operation to itself.
-	 */
-	public void selfAdd(DenseVector vec) {
-		assert size == vec.size;
-		
-		for (int i = 0; i < size; i ++) 
-			this.data[i] = this.data[i] + vec.data[i];
-	}
+        DenseVector result = new DenseVector(size);
+        for (int i = 0; i < size; i++)
+            result.data[i] = this.data[i] * val;
 
-	/**
-	 * Do vector operation: {@code a - b}
-	 * 
-	 * @return a dense vector with results of {@code c = a - b}
-	 */
-	public DenseVector minus(DenseVector vec) {
-		assert size == vec.size;
+        return result;
+    }
 
-		DenseVector result = new DenseVector(size);
-		for (int i = 0; i < vec.size; i++)
-			result.data[i] = this.data[i] - vec.data[i];
+    public void selfScale(double val) {
+        for (int i = 0; i < size; i++)
+            this.data[i] = this.data[i] * val;
+    }
 
-		return result;
-	}
+    /**
+     * Do vector operation: {@code a + b}
+     *
+     * @return a dense vector with results of {@code c = a + b}
+     */
+    public DenseVector add(DenseVector vec) {
+        assert size == vec.size;
 
-	/**
-	 * Do vector operation: {@code a^t * b}
-	 * 
-	 * @return the inner product of two vectors
-	 */
-	public double inner(DenseVector vec) {
-		assert size == vec.size;
+        DenseVector result = new DenseVector(size);
+        for (int i = 0; i < result.size; i++)
+            result.data[i] = this.data[i] + vec.data[i];
 
-		double result = 0;
-		for (int i = 0; i < vec.size; i++)
-			result += get(i) * vec.get(i);
+        return result;
+    }
 
-		return result;
-	}
+    /**
+     * Vector add operation to itself.
+     */
+    public void selfAdd(DenseVector vec) {
+        assert size == vec.size;
 
-	/**
-	 * Do vector operation: {@code a * b^t}
-	 * 
-	 * @return the outer product of two vectors
-	 */
-	public DenseMatrix outer(DenseVector vec) {
-		DenseMatrix mat = new DenseMatrix(this.size, vec.size);
+        for (int i = 0; i < size; i++)
+            this.data[i] = this.data[i] + vec.data[i];
+    }
 
-		for (int i = 0; i < mat.numRows; i++)
-			for (int j = 0; j < mat.numColumns; j++)
-				mat.set(i, j, get(i) * vec.get(j));
+    /**
+     * Do vector operation: {@code a - b}
+     *
+     * @return a dense vector with results of {@code c = a - b}
+     */
+    public DenseVector minus(DenseVector vec) {
+        assert size == vec.size;
 
-		return mat;
-	}
+        DenseVector result = new DenseVector(size);
+        for (int i = 0; i < vec.size; i++)
+            result.data[i] = this.data[i] - vec.data[i];
 
-	@Override
-	public String toString() {
-		return Strings.toString(data);
-	}
+        return result;
+    }
+
+    /**
+     * Do vector operation: {@code a^t * b}
+     *
+     * @return the inner product of two vectors
+     */
+    public double inner(DenseVector vec) {
+        assert size == vec.size;
+
+        double result = 0;
+        for (int i = 0; i < vec.size; i++)
+            result += get(i) * vec.get(i);
+
+        return result;
+    }
+
+    /**
+     * Do vector operation: {@code a * b^t}
+     *
+     * @return the outer product of two vectors
+     */
+    public DenseMatrix outer(DenseVector vec) {
+        DenseMatrix mat = new DenseMatrix(this.size, vec.size);
+
+        for (int i = 0; i < mat.numRows; i++)
+            for (int j = 0; j < mat.numColumns; j++)
+                mat.set(i, j, get(i) * vec.get(j));
+
+        return mat;
+    }
+
+    @Override
+    public String toString() {
+        return Strings.toString(data);
+    }
 
 }
--- src/main/main_online.java
+++ src/main/main_online.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.IOException;
+import java.util.Date;
 
 import data_structure.DenseMatrix;
 import utils.Printer;
@@ -11,80 +12,94 @@ import algorithms.ItemPopularity;
 import algorithms.MFbpr;
 
 public class main_online extends main {
-	public static void main(String argv[]) throws IOException {
-		String dataset_name = "yelp";
-		String method = "FastALS";
-		int interval = 1000;
-		double w0 = 512;
-		int factors = 64;
-		int maxIter = 50;
-		int maxIterOnline = 1;
-		double alpha = 0.4;
-		String onlineMode = "ui";
-		double w_new = 1;
-		
-		if (argv.length > 0) {
-			dataset_name = argv[0];
-			method = argv[1];
-			interval = Integer.parseInt(argv[2]);
-			w0 = Double.parseDouble(argv[3]);
-			factors = Integer.parseInt(argv[4]);
-			maxIter = Integer.parseInt(argv[5]);
-			maxIterOnline = Integer.parseInt(argv[6]);
-			alpha = Double.parseDouble(argv[7]);
-			if (argv.length >= 9)	onlineMode = argv[8];
-			if (argv.length >= 10) w_new = Double.parseDouble(argv[9]);
-		}
-		ReadRatings_GlobalSplit("data/" + dataset_name + ".rating", 0.1);
-		
-		System.out.printf("Online evaluation for %s: factors=%d, maxIter=%d, maxInterOnline=%d, interval=%d, onlineMode(bpr only)=%s\n",
-				method, factors, maxIter, maxIterOnline, interval, onlineMode);
-		System.out.println("====================================================");
-		
-		ItemPopularity popularity = new ItemPopularity(trainMatrix, testRatings, topK, threadNum);
-		evaluate_model_online(popularity, "Popularity", interval);
-		
-		double init_mean = 0;
-		double init_stdev = 0.01;
-		double reg = 0.01;
-		boolean showProgress = false;
-		boolean showLoss = false;
-		
-		// Remove ALS is not suitable for online learning.
-		if (method.equalsIgnoreCase("als")) {
-			MF_ALS als = new MF_ALS(trainMatrix, testRatings, topK, threadNum,
-					factors, maxIter, w0, reg, init_mean, init_stdev, showProgress, showLoss);
-			als.buildModel();
-			als.maxIterOnline = maxIterOnline;
-			evaluate_model_online(als, "MF_ALS", interval);
-		}
-		
-		if (method.equalsIgnoreCase("fastals")) {
-			MF_fastALS fals = new MF_fastALS(trainMatrix, testRatings, topK, threadNum,
-					factors, maxIter, w0, alpha, reg, init_mean, init_stdev, showProgress, showLoss);
-			fals.w_new = w_new;
-			fals.buildModel();
-			fals.maxIterOnline = maxIterOnline;
-			evaluate_model_online(fals, "MF_fastALS", interval);
-		}
-		
-		if (method.equalsIgnoreCase("cd")) {
-			MF_CD cd = new MF_CD(trainMatrix, testRatings, topK, threadNum,
-					factors, maxIter, w0, reg, init_mean, init_stdev, showProgress, showLoss);
-			cd.w_new = w_new;
-			cd.buildModel();
-			cd.maxIterOnline = maxIterOnline;
-			evaluate_model_online(cd, "MF_CD", interval);
-		}
-		
-		if (method.equalsIgnoreCase("bpr")) {
-			MFbpr bpr = new MFbpr(trainMatrix, testRatings, topK, threadNum, 
-					factors, maxIter, 0.01, false, reg, init_mean, init_stdev, 1, showProgress);
-			bpr.onlineMode = onlineMode;
-			bpr.buildModel();
-			bpr.maxIterOnline = maxIterOnline;
-			evaluate_model_online(bpr, "BPR", interval);
-		}
-	
-	} // end main
+    public static void main(String argv[]) throws IOException {
+        String dataset_name = "yelp";
+        String method = "FastALS";
+        int interval = 1000;
+        double w0 = 512;
+        int factors = 64;
+        int maxIter = 50;
+        int maxIterOnline = 1;
+        double alpha = 0.4;
+        String onlineMode = "ui";
+        double w_new = 1;
+
+        //从控制台中输入的参数
+        if (argv.length > 0) {
+            dataset_name = argv[0];
+            method = argv[1];
+            interval = Integer.parseInt(argv[2]);
+            w0 = Double.parseDouble(argv[3]);
+            factors = Integer.parseInt(argv[4]);
+            maxIter = Integer.parseInt(argv[5]);
+            maxIterOnline = Integer.parseInt(argv[6]);
+            alpha = Double.parseDouble(argv[7]);
+            if (argv.length >= 9) onlineMode = argv[8];
+            if (argv.length >= 10) w_new = Double.parseDouble(argv[9]);
+        }
+
+        //按照时间先后顺序划分dataset
+        ReadRatings_GlobalSplit("data/" + dataset_name + ".rating", 0.1);
+
+        System.out.printf("Online evaluation for %s: factors=%d, maxIter=%d, maxInterOnline=%d, interval=%d, onlineMode(bpr only)=%s\n",
+                method, factors, maxIter, maxIterOnline, interval, onlineMode);//todo ??? onlineMode为什么bpr only ???
+        System.out.println("====================================================");
+
+        //todo popularity用在哪里了???，注销了也没什么影响呀
+        //todo 是MostPopular的推荐算法？？？貌似是的。。。
+//        ItemPopularity popularity = new ItemPopularity(trainMatrix, testRatings, topK, threadNum);
+//        evaluate_model_online(popularity, "Popularity", interval);
+
+        double init_mean = 0;
+        double init_stdev = 0.01;
+        double reg = 0.01;
+        boolean showProgress = false;
+        boolean showLoss = false;
+
+        // Remove ALS is not suitable for online learning.
+        if (method.equalsIgnoreCase("als")) {
+            MF_ALS als = new MF_ALS(trainMatrix, testRatings, topK, threadNum,
+                    factors, maxIter, w0, reg, init_mean, init_stdev, showProgress, showLoss);
+            als.buildModel();
+            als.maxIterOnline = maxIterOnline;
+            evaluate_model_online(als, "MF_ALS", interval);
+        }
+
+        /**
+         * todo ----------------------------------------------------------------------------------------------
+         */
+        if (method.equalsIgnoreCase("fastals")) {
+            MF_fastALS fals = new MF_fastALS(trainMatrix, testRatings, topK, threadNum,
+                    factors, maxIter, w0, alpha, reg, init_mean, init_stdev, showProgress, showLoss);
+            fals.w_new = w_new;
+            long start = System.currentTimeMillis();
+            System.out.println("train start!");
+            fals.buildModel();
+            System.out.println("train cost:" + Printer.printTime(start - System.currentTimeMillis()));
+            fals.maxIterOnline = maxIterOnline;
+            evaluate_model_online(fals, "MF_fastALS", interval);
+        }
+        /**
+         * todo ----------------------------------------------------------------------------------------------
+         */
+
+        if (method.equalsIgnoreCase("cd")) {
+            MF_CD cd = new MF_CD(trainMatrix, testRatings, topK, threadNum,
+                    factors, maxIter, w0, reg, init_mean, init_stdev, showProgress, showLoss);
+            cd.w_new = w_new;
+            cd.buildModel();
+            cd.maxIterOnline = maxIterOnline;
+            evaluate_model_online(cd, "MF_CD", interval);
+        }
+
+        if (method.equalsIgnoreCase("bpr")) {
+            MFbpr bpr = new MFbpr(trainMatrix, testRatings, topK, threadNum,
+                    factors, maxIter, 0.01, false, reg, init_mean, init_stdev, 1, showProgress);
+            bpr.onlineMode = onlineMode;
+            bpr.buildModel();
+            bpr.maxIterOnline = maxIterOnline;
+            evaluate_model_online(bpr, "BPR", interval);
+        }
+
+    } // end main
 }
